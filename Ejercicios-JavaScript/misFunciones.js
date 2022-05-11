02d5a314b0e215c1ca9c1a64450cdb9fa0b8bbd5
manuelasimes <2103975@ucc.edu.ar>
2022-05-11 08:46:41 -0300

    ejerciciio 2 js

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,17 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function CambioUnidades (unidad, valor)
+{
+    if (isNaN(valor)){
+        alert("Se ingreso un valor invalido en " + unidad);
+        document.lasUnidades.unid_metro.value= "";
+        document.lasUnidades.unid_pulgada.value= "";
+        document.lasUnidades.unid_pie.value= "";
+        document.lasUnidades.unid_yarda.value= "";
+    }else if (unidad === "metro") {
+        document.lasUnidades.unid_pulgada.value = valor *39.3701;
+        document.lasUnidades.unid_pie.value = valor *3.28984;
+        document.lasUnidades.yarda. value= valor *1.89361;
+    }
+}
